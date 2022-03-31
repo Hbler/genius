52d8ba63fe41bcb491069ef475a0541870291fb8
Hugo Ernesto <mail.hbler@gmail.com>
2022-03-31 00:18:11 -0300

    update: sistema de pontos e rodadas pronto

--- assets/module/game.js
+++ assets/module/game.js
@@ -5,27 +5,25 @@ import { currentSequence } from "./main.js";
 
 //// Global Variables
 /// Display Elements
-const Board = document.getElementById("board");
 const Score = document.getElementById("score-board");
+const Points = Score.childNodes[1];
+let currentPoints = +Points.innerText.split(" ")[1];
+const Rounds = Score.childNodes[3];
+let currentRound = +Rounds.innerText.split(" ")[1];
 const Summary = document.getElementById("summary");
 const sMessage = document.getElementById("sum-message");
 /// Buttons
 const StartGame = document.getElementById("start");
-const Reset = document.getElementById("reset");
 const TryAgain = document.getElementById("try");
+const Reset = document.getElementById("reset");
 const NewGame = document.getElementById("new");
 /// Gameplay
+let Difficulty;
 let playerClick = 0;
-let Round = 1;
-let Points = 0;
 
 //// Functions
 function randomSequecer(cS) {
-  let difficulty;
-  Diffs.forEach((d) => {
-    if (d.checked) difficulty = d.value;
-  });
-  const source = PossibleColors[`${difficulty}`];
+  const source = PossibleColors[`${Difficulty}`];
   const nextColor = source[Math.floor(Math.random() * source.length)];
   cS.push(nextColor);
 }
@@ -60,9 +58,54 @@ function gameMessages() {
   }
 }
 
+function updatePoints() {
+  let roundPoint;
+
+  switch (Difficulty) {
+    case "normal":
+      roundPoint = 1;
+      break;
+    case "medium":
+      roundPoint = 2;
+      break;
+    case "hard":
+      roundPoint = 3;
+      break;
+  }
+
+  currentPoints += roundPoint;
+
+  switch (true) {
+    case currentPoints < 10:
+      Points.innerText = `Pontos: 00${currentPoints}`;
+      break;
+    case currentPoints > 9:
+      Points.innerText = `Pontos: 0${currentPoints}`;
+      break;
+    case currentPoints > 99:
+      Points.innerText = `Pontos: ${currentPoints}`;
+  }
+}
+
+function updateRounds() {
+  currentRound++;
+  Rounds.innerText = `Rodada: ${currentRound}`;
+}
+
+function checkForClear(node, state) {
+  switch (state) {
+    case "contain":
+      if (node.classList.contains("clear")) node.classList.toggle("clear");
+      break;
+    case "not":
+      if (!node.classList.contains("clear")) node.classList.toggle("clear");
+      break;
+  }
+}
+
 function createSummary() {
   sMessage.innerHTML = "";
-  sMessage.innerHTML = `<h3>Fim da Partida</h3><p>Nessa partida você chegou na ${Round}ª rodada!</p><p>E conseguiu ${Points} pontos</p><p>Agora você pode <span>tentar novamente</span> em uma nova partida na mesma ou em outra dificuldade, ou <span>zerar o placar</span> e começar um novo jogo.</p>`;
+  sMessage.innerHTML = `<h3>Fim da Partida</h3><p>Nessa partida você chegou na ${currentRound}ª rodada!</p><p>E conseguiu ${currentPoints} pontos</p><p>Agora você pode <span>tentar novamente</span> em uma nova partida, na mesma ou em outra dificuldade, e continuar pontuando...</p> <p>Ou <span>zerar o placar</span> e começar um novo jogo.</p>`;
 }
 
 function checkMatch() {
@@ -72,7 +115,9 @@ function checkMatch() {
 
   if (colorClicked === cS[pC]) {
     playerClick++;
+    updatePoints();
     if (pC === cS.length - 1) {
+      updateRounds();
       gameMessages();
       playerClick = 0;
       play();
@@ -83,13 +128,38 @@ function checkMatch() {
   }
 }
 
+function reset() {
+  Points.innerText = "Pontos: 000";
+  Rounds.innerText = "Rodada: 1";
+}
+
 function play() {
-  // Setup
-  if (!Summary.classList.contains("clear")) {
-    Summary.classList.toggle("clear");
-    currentSequence.splice(0, currentSequence.length);
-    playerClick = 0;
+  // Set difficulty
+  Diffs.forEach((d) => {
+    if (d.checked) Difficulty = d.value;
+  });
+
+  // Reset
+  if (this !== undefined) {
+    if (this.id === "start" || this.id === "try") {
+      checkForClear(Reset, "contain");
+      checkForClear(Rounds, "contain");
+      currentSequence.splice(0, currentSequence.length);
+      Rounds.innerText = "Rodada: 1";
+      playerClick = 0;
+    }
+    if (this.id === "reset" || this.id === "new") {
+      checkForClear(Rounds, "not");
+      checkForClear(Reset, "not");
+      checkForClear(Summary, "not");
+      currentSequence.splice(0, currentSequence.length);
+      playerClick = 0;
+      reset();
+      return;
+    }
   }
+
+  checkForClear(Summary, "not");
 
   // Gameplay
   randomSequecer(currentSequence);
@@ -98,4 +168,4 @@ function play() {
 }
 
 //// Export
-export { Score, StartGame, TryAgain, checkMatch, play };
+export { Score, StartGame, TryAgain, Reset, NewGame, checkMatch, play };
--- assets/module/main.js
+++ assets/module/main.js
@@ -1,22 +1,18 @@
 //// Imports
 import { getDiff } from "./layout_functions.js";
-import { Score, StartGame, TryAgain, play } from "./game.js";
+import { StartGame, TryAgain, Reset, NewGame, play } from "./game.js";
 
 //// Global Variables
-const Points = Score.childNodes[1];
-let currentPoints = +Points.innerText.split(" ")[1];
-const Rounds = Score.childNodes[3];
-let currentRound = +Rounds.innerText.split(" ")[1];
 const currentSequence = [];
 
 //// Listeners
 StartGame.addEventListener("click", play);
 TryAgain.addEventListener("click", play);
+Reset.addEventListener("click", play);
+NewGame.addEventListener("click", play);
 
 //// Functions
 getDiff();
-
-console.log(currentPoints, currentRound);
 
 //// Export
 export { currentSequence };
